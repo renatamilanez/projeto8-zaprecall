8d413a5c09b51a74d3c0d3ea2f497e84d69f650e
Renata Milanez <renatamilanez.dev@gmail.com>
2022-07-25 15:54:52 -0300

    correção de bugs do css

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -10,51 +10,35 @@ export default function FirstPage({logged, setLogged}){
         {
             title: 'O que é JSX?',
             answer: 'Uma extensão da linguagem do JavaScript',
-            isAnswered: false,
-            icon: null
         },
         {
             title: 'O React é...',
             answer: 'uma biblioteca de linguagem do JavaScript',
-            isAnswered: false,
-            icon: null
         },
         {
             title: 'Componentes devem iniciar com...',
             answer: 'letra maiúscula',
-            isAnswered: false,
-            icon: null
         },
         {
             title: 'O ReactDOM nos ajuda...',
             answer: 'interagindo com a DOM para colocar componentes React na mesma',
-            isAnswered: false,
-            icon: null
         },
         {
             title: 'Podemos colocar ______ dentro do JSX',
             answer: 'expressões',
-            isAnswered: false,
-            icon: null
             
         },
         {
             title: 'Usamos o npm para...',
             answer: 'gerenciar os pacotes necessários e suas dependências',
-            isAnswered: false,
-            icon: null
         },
         {
             title: 'Usamos props para...',
             answer: 'passar diferentes informações para componentes',
-            isAnswered: false,
-            icon: null
         },
         {
             title: 'Usamos estado (state) para...',
             answer: 'dizer para o React quais informações, quando atualizadas, devem renderizar a tela novamente',
-            isAnswered: false,
-            icon: null
         },
     ];
     
